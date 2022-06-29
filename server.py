d7f7de99bdbbed47cab3f8adc16253679340b48b
romankravchuk <kravchuk.roman02@gmail.com>
2022-06-29 18:54:52 +0800

    improove rpc server

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import asyncio
+import json
+
+from uuid import UUID
 
 from aio_pika import Connection, Message
 from aio_pika.abc import (
@@ -13,47 +16,107 @@ from logger import logger
 
 
 class RpcServer(object):
-    connection: AbstractConnection
-    channel: AbstractChannel
-    queue: AbstractQueue
-    exchange: AbstractExchange
-    loop: asyncio.AbstractEventLoop
+    _connection: AbstractConnection
+    _channel: AbstractChannel
+    _queue: AbstractQueue
+    _exchange: AbstractExchange
+    _loop: asyncio.AbstractEventLoop
+    _clients: dict[str: UUID]
 
-    def __init__(self):
-        self.loop = asyncio.get_running_loop()
+    def __init__(self) -> None:
+        self._loop = asyncio.get_running_loop()
+        self._clients = {}
 
     async def _connect(self) -> None:
-        self.connection = Connection(
+        self._connection = Connection(
             "amqp://server:test@localhost/",
-            loop=self.loop
+            loop=self._loop
         )
-        self.connection.connect()
-        self.channel = await self.connection.channel()
-        self.exchange = self.channel.default_exchange
-        self.queue = await self.channel.declare_queue('rpc_queue')
+        await self._connection.connect()
+        self._channel = await self._connection.channel()
+        self._exchange = self._channel.default_exchange
+        self._queue = await self._channel.declare_queue('rpc_queue')
+        logger.info(f'Сервер подключился к {self._connection.url} без ошибок.')
 
-    async def _get_response(content: str):
-        pass
+    async def _send(self, client: dict, response: str) -> None:
+        self._exchange.publish(
+            Message(
+                correlation_id=client['correlation_id'],
+                content_type='application/json',
+                body=json.dumps(response).encode()
+            ),
+            routing_key=client['name']
+        )
 
-    async def run_until_complete(self):
+    async def _get_response(self, content: str) -> str:
+        data = json.loads(content)        
+        response = await self._execute_command(data=data)
+        return response
+
+    async def _close(self) -> dict:
+        self._connection.close()
+        return { 'command': 'close' }
+
+    async def _execute_command(self, data: dict):
+        response: dict[str, str] = {}
+
+        match data['command']:
+            case 'init_client':
+                pass
+            case 'get_all':
+                pass
+            case 'add':
+                pass
+            case 'delete':
+                pass
+            case 'find_by_author':
+                pass
+            case 'find_by_name':
+                pass
+            case 'bye':
+                pass
+            case 'stop':
+                response = await self._close()
+            case _:
+                logger.info(data)
+        
+        return json.dumps(response)
+
+    async def _stop(self):
+        await self._loop.close()
+        logger.info('Сервер выключен.')
+
+    async def run_until_complete(self) -> None:
         await self._connect()
 
-        async with self.queue.iterator() as iterator:
+        async with self._queue.iterator() as iterator:
             message: AbstractIncomingMessage
             async for message in iterator:
                 try:
                     async with message.process(requeue=False):
                         assert message.reply_to is not None
-
                         content = message.body.decode()
-                        self._get_response(content=content)
+                        
+                        client = {
+                            'id': content['id'],
+                            'name': message.reply_to,
+                            'correlation_id': message.correlation_id,
+                        }
+
+                        response = await self._get_response(content=content)
+                        logger.info(f'Получен ответ для {client["name"]}: {response}')
+
+                        await self._send(client=client,response=response)
+                        logger.info(f'Ответ для {client["name"]} успешно отправлен')
+
+                        if response['command'] == 'close':
+                            await self._stop()
                 except Exception as e:
                     logger.error(e)
 
 
 async def main() -> None:
-    print(' [x] Ожидание запросов.')
-    RpcServer().run_until_complete()
+    await RpcServer().run_until_complete()
 
 if __name__ == "__main__":
     asyncio.run(main())
